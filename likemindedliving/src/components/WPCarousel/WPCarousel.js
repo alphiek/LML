8e9f307100e22795fa30d9312fdb725579196a24
alphiek <43937504+alphiek@users.noreply.github.com>
2019-10-20 19:39:28 +0200

    Update Carousel

--- likemindedliving/src/components/WPCarousel/WPCarousel.js
+++ likemindedliving/src/components/WPCarousel/WPCarousel.js
@@ -8,20 +8,7 @@ export const CardContainer = styled.li`
   display: flex;
   flex-wrap: wrap;
   border-radius: 16px;
-  transition: all .2s ease-in-out;
   box-shadow: 0 10px 18px -3px rgba(75, 74, 104, 0.25), 0 4px 6px -2px rgba(75, 74, 104, 0.05);
-  :hover {
-    box-shadow: 0 25px 25px -15px rgba(75, 74, 104, 0.45);
-    transform: scale(1.02);
-    transform: translate(0, -0.1em);
-    @media (max-width: 768px) {
-    box-shadow: 0 10px 18px -3px rgba(75, 74, 104, 0.25), 0 4px 6px -2px rgba(75, 74, 104, 0.05);
-    transform: none;
-    }
-  }
-  @media(max-width: 768px) {
-      transition: none;
-    }
   }
 `
 
